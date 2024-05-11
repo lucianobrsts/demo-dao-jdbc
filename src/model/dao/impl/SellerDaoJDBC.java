22f64008771d7c363df09695f00a019ee2b18817
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-05-11 08:49:09 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -56,6 +56,8 @@ public class SellerDaoJDBC implements SellerDao {
 				Department dep = instantiateDepartment(rs);
 				
 				Seller seller = instantiateSeller(rs, dep);
+				
+				return seller;
 			}
 			return null;
 			
